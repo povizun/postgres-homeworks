25d521f06c136dc032a30c7951cee2410c42c881
Futeki <Povizun@bk.ru>
2024-02-16 13:29:40 +0300

    first homework fix, added database.ini and config.py

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,11 +1,10 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
 from csv import DictReader
 from pathlib import Path
-
+from config import load_config
 import psycopg2
 
-password = input("Введите пароль от postgres: ")
-conn = psycopg2.connect(host='localhost', database='north', user='postgres', password=password)
+conn = psycopg2.connect(**load_config())
 try:
     with conn:
         with conn.cursor() as cur:
@@ -14,7 +13,7 @@ try:
                 for row in data:
                     cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)",
                                 (row["employee_id"], row["first_name"], row["last_name"],
-                                row["title"], row["birth_date"], row["notes"]))
+                                 row["title"], row["birth_date"], row["notes"]))
 
             with open(Path(__file__).parent / "north_data" / "customers_data.csv") as file:
                 data = DictReader(file)
@@ -27,6 +26,6 @@ try:
                 for row in data:
                     cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)",
                                 (row["order_id"], row["customer_id"], row["employee_id"],
-                                row["order_date"], row["ship_city"]))
+                                 row["order_date"], row["ship_city"]))
 finally:
     conn.close()
